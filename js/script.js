d5e241b9ce180b8e422470bae3d7b7b99509e570
zaturnvalley <zacharycmason@gmail.com>
2016-12-15 17:00:57 -0800

    addRun function test

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).one('pageinit', function(){
+  // Add Handler, tap is mobile for click
+  $('#submitAdd').on('tap', addRun);
+
+  /* 
+  * Add a run
+  */
+  function addRun(){
+    alert(1);
+  }
+});
